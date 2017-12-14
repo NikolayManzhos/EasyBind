60ab7a36403692c3a9ab8e37d5487e6e035d7e0e
NikolayManzhos <NikolayManzhos@users.noreply.github.com>
2017-12-15 01:41:58 +0300

    WIP

---
+++ upload_to_bintray.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -xe
+
+# You can run it from any directory.
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+"$PROJECT_DIR"/gradlew -Dbintray_email=$1 -Dbintray_user=$2 -Dbintray_key=$3 :easybind-annotations:bintrayUpload :easybind-compiler:bintrayUpload :easybind:bintrayUpload
